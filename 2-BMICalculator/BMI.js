56930f0bb956773f0a7e2a2b0df0737cf61216ee
Sachin <sachnsingh60051gmail.com>
2024-09-09 23:05:43 +0530

    suggestion added

--- 2-BMICalculator/BMI.js
+++ 2-BMICalculator/BMI.js
@@ -8,15 +8,26 @@ form.addEventListener('submit', function (e) {
   const height = parseInt(document.querySelector('#height').value);
   const weight = parseInt(document.querySelector('#weight').value);
   const results = document.querySelector('#results');
-
+  const outcome=document.querySelector('#outcome');
+ 
   if (height === '' || height < 0 || isNaN(height)) {
     results.innerHTML = `Please give a valid height ${height}`;
   } else if (weight === '' || weight < 0 || isNaN(weight)) {
     results.innerHTML = `Please give a valid weight ${weight}`;
   } else {
-    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    const bmi = parseFloat((weight / ((height * height) / 10000)).toFixed(2));
     //show the result
     results.innerHTML = `<span>${bmi}</span>`;
+    if(bmi<18.6){
+        outcome.innerHTML='you are under weight';
+     }
+     else if(bmi>=18.6 && bmi<=24.9){
+       outcome.innerHTML='you are normal';
+     }
+     else{
+       outcome.innerHTML='you are over weight';
+     }
   }
+
 });
 
